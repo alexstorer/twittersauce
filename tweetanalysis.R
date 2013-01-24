6f75c01ebfdbb74972652cbd5803ab674702f1ac
Alex Storer <alexstorer@gmail.com>
2013-01-24 14:10:26 -0500

    These are the updates

--- tweetanalysis.R
+++ tweetanalysis.R
@@ -12,77 +12,53 @@ Sys.setenv(NOAWT=TRUE)
 # This is a workaround for Macs
 
 library(tm) 
-#library(Snowball) 
 library(RWeka) 
-#library(rJava) 
-#library(RWekajars) 
 library(Rgraphviz)
-
-#library("topicmodels")
-#library("XML")
+library(slam)
 
 readvanilla <- function (elem, language, id) 
 {
-  # ideally, add in metadata for each piece of lexisnexis
+  # ideally, add in metadata for each document as well
   doc <- PlainTextDocument(elem$content, id = id, language = language)
 }
 
-tweets <- read.csv('/Users/astorer/Work/presentations/twitter/example_tweets.csv')
+# Load in the tweets
+tweets <- read.csv('/Users/astorer/Work/presentations/twitter/finalcode/twittersauce/example_tweets.csv')
+
+# Read just the tweet text into the corpus
 corpus <- Corpus(VectorSource(tweets$text),readerControl=list(reader = readvanilla))
 
-
+# This should turn off locale-specific sorting
 Sys.setlocale("LC_COLLATE", "C")
 
+# Make our document-term matrix
 dtm <- DocumentTermMatrix(corpus, control = list(stemming = FALSE,
                                                  tokenize = WordTokenizer,
                                                  stopwords = TRUE, 
                                                  minWordLength = 3,
                                                  removeNumbers = TRUE, 
                                                  removePunctuation = TRUE))
+
+# How many documents and terms do we have?
 dim(dtm)
 
+# What are the frequent terms?
+ft <- findFreqTerms(dtm, lowfreq = 150, highfreq = 5000)
 
-ft <- findFreqTerms(dtm, lowfreq = 150, highfreq = 500)
+# We can get the most frequent terms
+sorted_terms <- sort(col_sums(dtm),decreasing=T)
 
-plot(dtm, terms = ft, corThreshold = 0.05)
+# We can plot them
+barplot(sorted_terms[1:20],las=2)
 
-summary(col_sums(dtm))
-term_tfidf <-
-  tapply(dtm$v/row_sums(dtm)[dtm$i], dtm$j, mean) *
-  log2(nDocs(dtm)/col_sums(dtm > 0))
-summary(term_tfidf)
-sub_dtm <- dtm[,term_tfidf >= 0.1]
-sub_dtm <- sub_dtm[row_sums(sub_dtm) > 0,]
-summary(col_sums(sub_dtm))
+# Here are the 100 most popular terms in alphabetical order:
+sort(names(sorted_terms[1:100]))
 
-dim(sub_dtm)
+# We can also plot the terms with lines indicating if they correlate
+plot(dtm, terms = names(sorted_terms[3:20]), corThreshold = 0.075)
 
-# 2 topics
+# Here are terms that correlate with debt
+findAssocs(dtm,term="debt",0.3)
 
-k <- 10
-SEED <- 2010
-sub_TM <-
-  list(VEM = LDA(sub_dtm, k = k, control = list(seed = SEED)),
-       VEM_fixed = LDA(sub_dtm, k = k,
-                       control = list(estimate.alpha = FALSE, seed = SEED)),
-       Gibbs = LDA(sub_dtm, k = k, method = "Gibbs",
-                   control = list(seed = SEED, burnin = 1000,
-                                  thin = 100, iter = 1000)),
-       CTM = CTM(sub_dtm, k = k,
-                 control = list(seed = SEED,
-                                var = list(tol = 10^-4), 
-                                em = list(tol = 10^-3))))
-
-sapply(sub_TM[1:2], slot, "alpha")
-
-Terms <- terms(sub_TM[["VEM"]], 5)
-
-
-combmachine <- function(x) {
-  if (length(x)<2) {
-    return(data.frame())
-  }
-  as.data.frame(t(combn(x,2)))
-}
-
-Reduce(rbind,lapply(l,combmachine))
+# Here is a plot of some debt terms and how they are related
+plot(dtm, terms = names(findAssocs(dtm,term="debt",0.2)), corThreshold = 0.30)
