d8bc7e689386c411a027d315b4b02fbadf987cd7
Alex Storer <alexstorer@gmail.com>
2013-03-20 10:13:46 -0400

    Streaming API can be used with OAuth

---
+++ getreach.py
@@ -0,0 +1,51 @@
+import twitter
+import json
+import pickle
+import time
+import random
+import sys
+import csv
+import twitter__login
+
+# log in, and collect statuses containing a keyword
+twitter_stream = twitter__login.login_stream()
+res = twitter_stream.statuses.filter(track='obama')
+
+# store the tweets in a list
+reslist = []
+
+# keep the possible metadata in a set
+tweetfields = set([u'favorited', u'in_reply_to_user_id', u'contributors', u'truncated', u'text', u'in_reply_to_status_id', u'user', u'geo', u'id', u'possibly_sensitive', u'retweeted_status', u'filter_level', u'created_at', u'retweeted', u'coordinates', u'in_reply_to_user_id_str', u'entities', u'in_reply_to_status_id_str', u'in_reply_to_screen_name', u'source', u'place', u'retweet_count', u'id_str'])
+
+# we know that we want a few things:
+# user id, followers, tweet itself, geocode
+
+# where do we store them?
+fname = '/users/astorer/Work/shoffman/example_tweets.csv'
+f = open(fname,'w')
+dw = csv.DictWriter(f,fieldnames=list(tweetfields))
+dw.writeheader()
+
+# where do we store them?
+fname = '/users/astorer/Work/shoffman/example_tweets_brief.csv'
+fb = open(fname,'w')
+dwb = csv.DictWriter(fb,fieldnames=['userid','tweet','followers','geocode'])
+dwb.writeheader()
+
+
+# collect all of the results
+for r in res:
+    print len(reslist)
+    reslist.append(r)
+    dw.writerow({k:v.encode('utf8') if isinstance(v,unicode) else v for k,v in r.items()})
+    brieftext = r['text']
+    if isinstance(brieftext,unicode):
+        brieftext = brieftext.encode('utf8')
+    briefd = {'userid':r['user']['id'],
+              'tweet':brieftext,
+              'followers':r['user']['followers_count'],
+              'geocode':r['geo']}
+    dwb.writerow(briefd)
+
+f.close()
+fb.close()
--- twitter__login.py
+++ twitter__login.py
@@ -32,14 +32,40 @@ def login():
                         auth=twitter.oauth.OAuth(oauth_token, oauth_token_secret,
                         CONSUMER_KEY, CONSUMER_SECRET))
 
+def login_stream():
+
+    # Go to http://twitter.com/apps/new to create an app and get these items
+    # See also http://dev.twitter.com/pages/oauth_single_token
+
+    APP_NAME = ''
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
+    TOKEN_FILE = 'out/twitter.oauth'
+
+    try:
+        (oauth_token, oauth_token_secret) = read_token_file(TOKEN_FILE)
+    except IOError, e:
+        (oauth_token, oauth_token_secret) = oauth_dance(APP_NAME, CONSUMER_KEY,
+                CONSUMER_SECRET)
+
+        if not os.path.isdir('out'):
+            os.mkdir('out')
+
+        write_token_file(TOKEN_FILE, oauth_token, oauth_token_secret)
+         
+    return twitter.TwitterStream(
+                        auth=twitter.oauth.OAuth(oauth_token, oauth_token_secret,
+                        CONSUMER_KEY, CONSUMER_SECRET))
+
+    
 def login1():
 
     # Go to http://twitter.com/apps/new to create an app and get these items
     # See also http://dev.twitter.com/pages/oauth_single_token
 
     APP_NAME = ''
-    CONSUMER_KEY = 'Vdmmn8FDQOy56xMCxAAgg'
-    CONSUMER_SECRET = '7nqfvRaY0BwpIhit5EHgqJ1VHkj9SLXjN4zX0Pjr8'
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
     TOKEN_FILE = 'out/twitter.oauth'
 
     try:
